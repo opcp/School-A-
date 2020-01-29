2925ab3d3654261f63538d8ee06e052254d7ceb4
Shan-Yi <a24215188@gmail.com>
2020-01-29 11:41:23 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Map from './components/Map'
+import Search_bar from './components/Search_bar'
+import Swipe from './components/Swipe'
+import Hot_School from './components/Hot_School'
+import Hot_Country from './components/Hot_Country'
+import Footer from './components/Footer'
+
+
+
+const App = () => {
+  return (
+   <>
+   <Navbar/>
+  <Map/>
+  <Search_bar/>
+  <Swipe/>
+  <Hot_School/>
+  <Hot_Country/>
+  <Footer/>
+   </>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+import './web.scss'
+
+// import BootstrapTest from './bootstrap/BootstrapTest'
+
+// ReactDom.render(<App />,document.getElementById('root'))
+
+ReactDom.render(<App />, document.getElementById('root'))
